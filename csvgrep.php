f2981c33fc628bffb2eaa88a7ed76d5044c26258
Christian Bolstad <christian@bolstad.se>
2014-09-19 12:15:53 +0200

    parse some basic params

--- csvgrep.php
+++ csvgrep.php
@@ -8,6 +8,8 @@ require "vendor/bolstad/csv-writer/src/CsvWriter/Simple.php";
 date_default_timezone_set("Europe/Stockholm");
 
 use CsvParser\Simple;
+use Aura\Cli\CliFactory;
+use Aura\Cli\Status;
 
 #$csvDumper = new SimpleCsvWrite( 'loggalogga' );
 
@@ -16,4 +18,27 @@ function handler($data) {
   print_r($data);
 }
 
-Simple::parseRowByRow('data/Sacramentorealestatetransactions.csv','handler');
+
+// get the context and stdio objects
+$cli_factory = new CliFactory;
+$context = $cli_factory->newContext($GLOBALS);
+$stdio = $cli_factory->newStdio();
+
+// define options and named arguments through getopt
+$options = ['verbose,v,file'];
+$getopt = $context->getopt($options);
+
+print_r($getopt);
+
+$filename = $getopt->get('--file');
+if (! $filename) {
+    // print an error
+    $stdio->errln("<<red>>Please give a name to say hello to<<reset>>.");
+    exit(Status::USAGE);
+}
+
+Simple::parseRowByRow($filename,'handler');
+
+
+// done!
+exit(Status::SUCCESS);
